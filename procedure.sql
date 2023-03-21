d1f5ac0d021527cc5fa8d441e6dffbb0ecff77b2
snoh6839 <116703016+snoh6839@users.noreply.github.com>
2023-03-21 12:56:47 +0900

    procedure

---
+++ procedure.sql
@@ -0,0 +1,12 @@
+-- DELIMITER $$
+-- 
+-- CREATE PROCEDURE test_proc (
+-- 	IN in_num INT )
+-- -- 	, OUT )
+-- BEGIN
+--     SELECT * FROM employees LIMIT in_num;
+-- END $$
+-- 
+-- DELIMITER ;
+
+-- CALL test_proc(10); 
